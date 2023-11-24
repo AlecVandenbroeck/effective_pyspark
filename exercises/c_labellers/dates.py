3a19bc24f7be442358fd96f744ab79d8a0457f55
Alec Van den broeck <alec.vandenbroeck@ugent.be>
2023-11-24 14:54:12 +0000

    finish exercises

--- exercises/c_labellers/dates.py
+++ exercises/c_labellers/dates.py
@@ -1,10 +1,16 @@
-import datetime
+from datetime import datetime
 
 from pyspark.sql import DataFrame
+from pyspark.sql.types import BooleanType
+from pyspark.sql.functions import col, udf
 
+import holidays
 
 def is_belgian_holiday(date: datetime.date) -> bool:
-    pass
+    if date is None:
+        return None
+    be_holidays = holidays.BE()
+    return date in be_holidays
 
 
 def label_weekend(
@@ -12,7 +18,9 @@ def label_weekend(
 ) -> DataFrame:
     """Adds a column indicating whether or not the attribute `colname`
     in the corresponding row is a weekend day."""
-    pass
+    is_weekend = udf(lambda z: z.weekday() in [5,6], BooleanType())
+    frame = frame.withColumn(new_colname, is_weekend(col(colname)))
+    return frame
 
 
 def label_holidays(
@@ -22,7 +30,9 @@ def label_holidays(
 ) -> DataFrame:
     """Adds a column indicating whether or not the column `colname`
     is a holiday."""
-    pass
+    is_holiday = udf(is_belgian_holiday, BooleanType())
+    frame = frame.withColumn(new_colname, is_holiday(col(colname)))
+    return frame
 
 
 def label_holidays2(
--- exercises/d_cleansers/clean_flights_starter.py
+++ exercises/d_cleansers/clean_flights_starter.py
@@ -18,47 +18,109 @@
 from pathlib import Path
 
 from pyspark.sql import SparkSession, DataFrame
+import pyspark.sql.functions as f
+from pyspark.sql.window import Window
+from pyspark.sql.types import BooleanType
 
 
 def read_data(path: Path):
     spark = SparkSession.builder.getOrCreate()
     return spark.read.csv(
         str(path),
-        # For a CSV, `inferSchema=False` means every column stays of the string
-        # type. There is no time wasted on inferring the schema, which is
-        # arguably not something you would depend on in production either.
-        inferSchema=False,
-        header=True,
-        # The dataset mixes two values for null: sometimes there's an empty attribute,
-        # which you will see in the CSV file as two neighboring commas. But there are
-        # also literal "null" strings, like in this sample: `420.0,null,,1.0,`
-        # The following option makes a literal null-string equivalent to the empty value.
-        nullValue="null",
+        sep=',',
+        header=True
     )
 
 
 def clean(frame: DataFrame) -> DataFrame:
+    frame = frame.drop('MONTH', 'DAY_OF_MONTH', 'CANCELLED', 'ORIGIN_AIRPORT_ID', 'DEST_AIRPORT_ID', 'ARR_DEL15')
+    frame = frame.drop('DEP_DEL15', 'DISTANCE_GROUP', 'ARR_DELAY_GROUP', 'DEP_DELAY_GROUP', 'DEP_TIME', 'ARR_TIME', 'ACTUAL_ELAPSED_TIME')
+    frame = frame.drop('_c44', 'FLIGHTS')
+    frame = frame.withColumnRenamed("FL_DATE", "FLIGHT_DATE")
+    frame = frame.withColumnRenamed("TAIL_NUM", "TAIL_ID")
+    frame = frame.withColumnRenamed("FL_NUM", "FLIGHT_NUMBER")
+    frame = frame.withColumnRenamed("CRS_DEP_TIME", "SCHEDULED_DEPARTURE_TIME")
+    frame = frame.withColumnRenamed("CRS_ARR_TIME", "SCHEDULED_ARRIVAL_TIME")
+    frame = frame.withColumnRenamed("FL_NUM", "FLIGHT_NUMBER")
+    frame = frame.withColumnRenamed("CRS_ELAPSED_TIME", "SCHEDULED_DURATION")
+    frame = frame.withColumn("FLIGHT_NUMBER",frame['FLIGHT_NUMBER'].cast('int'))
+    frame = frame.withColumn("DEP_DELAY",frame['DEP_DELAY'].cast('int'))
+    frame = frame.withColumn("DEP_DELAY_NEW",frame['DEP_DELAY_NEW'].cast('int'))
+    frame = frame.withColumn("TAXI_OUT",frame['TAXI_OUT'].cast('int'))
+    frame = frame.withColumn("TAXI_IN",frame['TAXI_IN'].cast('int'))
+    frame = frame.withColumn("ARR_DELAY",frame['ARR_DELAY'].cast('int'))
+    frame = frame.withColumn("ARR_DELAY_NEW",frame['ARR_DELAY_NEW'].cast('int'))
+    frame = frame.withColumn("DIVERTED",frame['DIVERTED'].cast('int'))
+    frame = frame.withColumn("SCHEDULED_DURATION",frame['SCHEDULED_DURATION'].cast('int'))
+    frame = frame.withColumn("AIR_TIME",frame['AIR_TIME'].cast('int'))
+    frame = frame.withColumn("DISTANCE",frame['DISTANCE'].cast('int'))
+    frame = frame.withColumn("CARRIER_DELAY",frame['CARRIER_DELAY'].cast('int'))
+    frame = frame.withColumn("NAS_DELAY",frame['NAS_DELAY'].cast('int'))
+    frame = frame.withColumn("WEATHER_DELAY",frame['WEATHER_DELAY'].cast('int'))
+    frame = frame.withColumn("DIVERTED",frame['DIVERTED'].cast(BooleanType()))
+    frame = frame.withColumn("SECURITY_DELAY",frame['SECURITY_DELAY'].cast('int'))
+    frame = frame.withColumn("LATE_AIRCRAFT_DELAY",frame['LATE_AIRCRAFT_DELAY'].cast('int'))
+    frame = frame.withColumn("NAS_DELAY",frame['NAS_DELAY'].cast('int'))
+    
     return frame
 
 
 if __name__ == "__main__":
     # use relative paths, so that the location of this project on your system
     # won't mean editing paths
-    path_to_exercises = Path(__file__).parents[1]
-    resources_dir = path_to_exercises / "resources"
-    target_dir = path_to_exercises / "target"
+    
+    resources_dir = Path("/workspace/effective_pyspark/exercises") / "resources"
+
     # Create the folder where the results of this script's ETL-pipeline will
     # be stored.
-    target_dir.mkdir(exist_ok=True)
+    #target_dir.mkdir(exist_ok=True)
 
     # Extract
-    frame = read_data(resources_dir / "flights")
+    frame = read_data(resources_dir / "flight" )
+    carriers = read_data(resources_dir / "carriers.csv")
+
     # Transform
     cleaned_frame = clean(frame)
-    # Load
-    cleaned_frame.write.parquet(
-        path=str(target_dir / "cleaned_flights"),
-        mode="overwrite",
-        # Exercise: how much bigger are the files when the compression codec is set to "uncompressed"? And 'gzip'?
-        compression="snappy",
-    )
+    cleaned_frame.show(5)
+
+    # Question 1
+    print("Question 1:")
+    q1 = cleaned_frame.filter("""UNIQUE_CARRIER == 'AA' AND YEAR == '2011'""")
+    print(q1.count())
+
+    # Question 2
+    print("Question 2:")
+    q2 = q1.filter("""ARR_DELAY <= 10""")
+    print(q2.count())
+    
+    # Question 3
+    print("Question 3:")
+    q3 = cleaned_frame.filter("""DEP_DELAY > 0""").select('DAY_OF_WEEK').groupBy('DAY_OF_WEEK')
+    q3.count().show()
+
+    # Question 4
+    print("Question 4:")
+    temp = cleaned_frame.filter("""YEAR == '2011'""")
+    cd = temp.filter(cleaned_frame['CARRIER_DELAY']!= 0).count()
+    wd = temp.filter(cleaned_frame['WEATHER_DELAY']!= 0).count()
+    nd = temp.filter(cleaned_frame['NAS_DELAY']!= 0).count()
+    sd = temp.filter(cleaned_frame['SECURITY_DELAY']!= 0).count()
+    ad = temp.filter(cleaned_frame['LATE_AIRCRAFT_DELAY']!= 0).count()
+
+    print(f'CARRIER_DELAYS: {cd}')
+    print(f'WEATHER_DELAYS: {wd}')
+    print(f'NAS_DELAYS: {nd}')
+    print(f'SECURITY_DELAYS: {sd}')
+    print(f'AIRCRAFT_DELAYS: {ad}')
+
+    # calculate stats
+    stat_frame = cleaned_frame.groupBy('UNIQUE_CARRIER').avg('CARRIER_DELAY')
+    w = Window.partitionBy('CARRIER').orderBy(carriers['START_DATE_SOURCE'].desc())
+
+    #adding rownumber to the data
+    carriers = carriers.withColumn("rn", f.row_number().over(w))
+    carriers = carriers.filter(carriers["rn"] == 1).select('CARRIER', 'CARRIER_NAME')
+
+    stat_frame = stat_frame.join(carriers, stat_frame.UNIQUE_CARRIER==carriers.CARRIER).select('CARRIER_NAME', 'avg(CARRIER_DELAY)')
+    stat_frame = stat_frame.sort(stat_frame['avg(CARRIER_DELAY)'].desc())
+    stat_frame.show(10)
